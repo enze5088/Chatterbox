7e35ea637051de37516cfc401061a8f9cfa76b6d
苗圃 <pu.miao@foxmail.com>
2023-04-13 11:53:42 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -22,8 +22,8 @@ torchrun  --nnodes=1 --nproc_per_node $NUM_GPU --master_port $PORT_ID train_bloo
     --cache_dir './cache/' \
     --overwrite_output_dir \
     --predict_with_generate \
-    --eval_steps 20000 \
-    --save_steps 20000 \
+    --eval_steps 5000 \
+    --save_steps 5000 \
     --logging_steps 10 \
     --max_eval_samples 50 \
     --max_source_length 768 \
