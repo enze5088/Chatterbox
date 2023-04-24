859c94123a09d3ba0263a38d52cfe10fe7f8f278
苗圃 <pu.miao@foxmail.com>
2023-04-24 19:39:21 +0800

    Update train.sh

--- train/train.sh
+++ train/train.sh
@@ -5,25 +5,25 @@ export OMP_NUM_THREADS=8
 torchrun  --nnodes=1 --nproc_per_node $NUM_GPU --master_port $PORT_ID train_clm.py \
     --fp16 \
     --deepspeed ./configs/ds_offload_without_config.json \
-    --model_name_or_path "./model_file/bloomz-1b-zh/"  \
+    --model_name_or_path "./model_file/LLaMA-zh-base/"  \
     --train_file "../data/datasets/train_datasets.csv" \
     --validation_file "../data/datasets/test_datasets.csv" \
-    --learning_rate 3e-5 \
+    --learning_rate 1e-4 \
     --do_train \
     --do_eval \
     --evaluation_strategy steps \
     --num_train_epochs 1 \
     --output_dir './result/total/' \
     --save_total_limit 3 \
-    --per_device_train_batch_size 8 \
-    --per_device_eval_batch_size 8 \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 16 \
     --cache_dir './cache/' \
     --overwrite_output_dir \
     --eval_steps 5000 \
     --save_steps 5000 \
     --logging_steps 10 \
     --max_eval_samples 50 \
-    --block_size 700 \
+    --block_size 256 \
     --preprocessing_num_workers 32 \
     --dataloader_num_workers 8 \
     "$@"
