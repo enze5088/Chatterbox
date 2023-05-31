17aa813aac4d57803255f4cd326e13df2424d057
苗圃 <pu.miao@foxmail.com>
2023-05-31 18:17:33 +0800

    20230531-3

--- example/TrainTokenizersExample/test_tokenizers.py
+++ example/TrainTokenizersExample/test_tokenizers.py
@@ -1,12 +1,41 @@
-from transformers import AutoTokenizer, LlamaTokenizerFast
+from transformers import AutoTokenizer,LlamaTokenizerFast
 model_path = "./output/"
-tokenizer = LlamaTokenizerFast(tokenizer_file="./BPE_tokenizer.json")
-tokenizer.save_pretrained('./llama/')
-tokenizer = LlamaTokenizerFast.from_pretrained('./llama/')
+tokenizer = LlamaTokenizerFast(tokenizer_file="./output/BPE_tokenizer.json",pad_token="[PAD]",)
+print(tokenizer)
+
+
+tokenizer.save_pretrained('./output/llama/')
+
+tokenizer = LlamaTokenizerFast.from_pretrained('./output/llama/')
+
 print(len(tokenizer))
 print(tokenizer)
+tokenizer.save_pretrained('./output/llama/')
+print('tokenize')
 content = '白日依山尽，黄河入海流.欲穷千里目,更上一层楼。'
+r = tokenizer.tokenize(content)
+print(tokenizer)
+print(len(content), len(r))
+print(r)
+
+content = '太太好啊，世界。我啊'
+r = tokenizer.tokenize(content)
+ids = tokenizer.encode(content)
+texts = [tokenizer.convert_tokens_to_string([m]) for m in r]
+print(texts)
+print(tokenizer)
+print(ids)
+print(len(content), len(r),len(ids))
+print(r)
+
+r = tokenizer.decode(tokenizer.encode_plus(content)['input_ids'],clean_up_tokenization_spaces=True)
+print(r)
+
+content = '<s> </s> Cảm ơn Hẹn gặp bạn sau'
 r = tokenizer.tokenize(content)
 print(r)
 r = tokenizer.decode(tokenizer.encode_plus(content)['input_ids'],clean_up_tokenization_spaces=True)
 print(r)
+
+print(tokenizer)
+print(len(tokenizer))
--- example/TrainTokenizersExample/train_tokenizers.py
+++ example/TrainTokenizersExample/train_tokenizers.py
@@ -1,30 +1,34 @@
+# coding=utf-8
 import tokenizers
 from datasets import load_dataset
 from tokenizers.normalizers import NFD, StripAccents
-from tokenizers.pre_tokenizers import Digits, ByteLevel, Punctuation, Whitespace, CharDelimiterSplit
+from tokenizers.pre_tokenizers import Digits, ByteLevel, Punctuation, Whitespace, CharDelimiterSplit, UnicodeScripts, \
+    Metaspace
 from tokenizers.trainers import BpeTrainer
 from tokenizers import Tokenizer, normalizers, pre_tokenizers, decoders
 from tokenizers.models import BPE
-
 
 tokenizer = Tokenizer(BPE())
 normalizer = normalizers.Sequence([NFD(), StripAccents()])
 tokenizer.normalizer = normalizer
 # 我们主要使用四类分割，空白，标点符号，数字，及Bytelevel
-tokenizer.pre_tokenizer = pre_tokenizers.Sequence([Whitespace(), Punctuation(), Digits(individual_digits=True), ByteLevel()])
+tokenizer.pre_tokenizer = pre_tokenizers.Sequence(
+    [Whitespace(), Punctuation(), Digits(individual_digits=True), UnicodeScripts(), ByteLevel(use_regex=False)])
 # 需要设置解码器为BPE
-tokenizer.decoder = decoders.ByteLevel(add_prefix_space=True, use_regex=True)
+tokenizer.decoder = decoders.ByteLevel(add_prefix_space=True, use_regex=False)
 # 字节级 BPE 可能在生成的令牌中包括空白。如果您不希望偏移量包含这些空格，那么必须使用这个 PostProcessor。
 tokenizer.post_processor = tokenizers.processors.ByteLevel()
+special_tokens = ["[PAD]", "<s>", "</s>"]
 
-dataset = load_dataset("TurboPascal/tokenizers_example_zh_en", cache_dir='./cache/')
+r = tokenizer.add_special_tokens(special_tokens)
+tokenizer.enable_padding()
 
+dataset = load_dataset('text', data_files={'train_file': "./train_data.csv"}, cache_dir='./cache/')
 
 def batch_iterator(batch_size=10000):
-    for i in range(0, len(dataset['train']), batch_size):
-        yield dataset['train'][i: i + batch_size]["text"]
+    for i in range(0, len(dataset['train_file']), batch_size):
+        yield dataset['train_file'][i: i + batch_size]["text"]
 
-special_tokens = ["[CLS]", "[SEP]", "[PAD]", "[MASK]", "<s>", "</s>", "<t>", "</t>"]
-trainer = BpeTrainer(special_tokens=special_tokens, vocab_size=54000)
-tokenizer.train_from_iterator(batch_iterator(), trainer=trainer, length=len(dataset['train']))
-tokenizer.save("./BPE_tokenizer.json")
+trainer = BpeTrainer(vocab_size=45999, special_tokens=special_tokens)
+tokenizer.train_from_iterator(batch_iterator(), trainer=trainer, length=len(dataset['train_file']))
+tokenizer.save("./output/BPE_tokenizer.json")
